75e445d8ff2de3ce1b764e9b1e13fa03fe7f9e69
AAR072 <53275317+AAR072@users.noreply.github.com>
2025-06-08 22:26:44 -0400

    feat: added tray minimizing

--- main.js
+++ main.js
@@ -1,28 +1,57 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron');
 const path = require('path');
 
-function createWindow() {
-	const win = new BrowserWindow({
-		width: 1280,
-		height: 800,
-    icon: path.join(__dirname, 'icon.png'),
-		webPreferences: {
-			nodeIntegration: false,
-			contextIsolation: true
-		}
-	});
-
-	win.loadURL('https://app.asana.com/');
-}
+let mainWindow;
+let tray = null;
 
 app.whenReady().then(() => {
-	createWindow();
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
 
-	app.on('activate', () => {
-		if (BrowserWindow.getAllWindows().length === 0) createWindow();
-	});
+  mainWindow.loadURL('https://app.asana.com/');
+
+  // Prevent app from quitting when window is closed
+  mainWindow.on('close', (event) => {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+  });
+
+  // Create system tray icon
+  const trayIcon = nativeImage.createFromPath(
+    path.join(__dirname, 'icon.png')
+  );
+
+  tray = new Tray(trayIcon);
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show Asana',
+      click: () => {
+        mainWindow.show();
+      }
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.isQuiting = true;
+        app.quit();
+      }
+    }
+  ]);
+
+  tray.setToolTip('Asana');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', () => {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+  });
 });
 
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') app.quit();
-});
